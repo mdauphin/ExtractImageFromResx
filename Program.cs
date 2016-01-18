20f9cf2d23bbbe65dbb51c31ce2dcbdd06cce226
mdauphin <mdauphin@viewpoint.fr>
2016-01-18 13:13:37 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace ExtractImageFromResx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var doc = new XmlDocument();
+            doc.Load(args[0]);
+
+            var root = doc.DocumentElement ;
+
+            XmlNodeList datas = root.SelectNodes("data");
+
+            foreach( XmlNode d in datas )
+            {
+                if ( d.Attributes["type"].Value == "System.Drawing.Bitmap, System.Drawing" ) 
+                {
+                    ExtractImage(d.FirstChild.InnerText, d.Attributes["name"].Value + ".png" );
+                }
+            }
+        }
+
+        private static void ExtractImage(string im_encode, string output_filename)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(im_encode);
+
+            using (var bw = new BinaryWriter(new FileStream(output_filename, FileMode.Create)))
+            {
+                bw.Write(base64EncodedBytes);
+            }
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+Extract image from resx visual studio 2005 file format
+
+Use:
+	ExtractImageFromResx.Exe path_to_resx_file 
+
+All images are extract in current folder
